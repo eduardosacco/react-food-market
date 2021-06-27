b9617fcfad030f822516d1e4a5b0777281037e94
Eduardo Sacco <saccoeduardo@gmail.com>
2021-06-27 01:30:58 -0300

    Rename CartContext. Add Remove functionality to CartProvider.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,10 +9,43 @@ const defaultCartState = {
 
 function cartReducer(state, action) {
     if (action.type === 'ADD') {
-        // not sure this logic takes into acount adding items with same id several times
-        const updatedItems = state.items.concat(action.item);
+        const existingItemIndex = state.items.findIndex(item => item.id === action.item.id);
+        const existingItem = state.items[existingItemIndex];
+
+        let updatedItems;
+        if (existingItem) {
+            const updatedItem = {...existingItem, amount: existingItem.amount + action.item.amount};
+            updatedItems = [...state.items];
+            updatedItems[existingItemIndex] = updatedItem;
+        }
+        else {
+            updatedItems = state.items.concat(action.item);
+        }
+        
         const updatedTotalAmount = state.totalAmount
             + action.item.price * action.item.amount;
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+
+    if (action.type === 'REMOVE') {
+        const existingItemIndex = state.items.findIndex(item => item.id === action.id);
+        const existingItem = state.items[existingItemIndex];
+
+        let updatedItems;
+        if (existingItem.amount > 1) {
+            const updatedItem = {...existingItem, amount: existingItem.amount - 1};
+            updatedItems = [...state.items];
+            updatedItems[existingItemIndex] = updatedItem;
+        }
+        else {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        }
+        
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
 
         return {
             items: updatedItems,
@@ -31,7 +64,7 @@ function CartProvider(props) {
     }
 
     function removeItemHandler(id) {
-        dispatchCartReducer({type:'REMOVE', item: id});
+        dispatchCartReducer({type:'REMOVE', id: id});
     }
 
     const cartContext = {
