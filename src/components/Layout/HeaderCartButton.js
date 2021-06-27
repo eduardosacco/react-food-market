cdbb9bbecb37215d4652a6377373e170d0e32942
Eduardo Sacco <saccoeduardo@gmail.com>
2021-06-27 01:47:55 -0300

    Add bump animation logic when adding products

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
 function HeaderCartButton(props) {
     const cartCtx = useContext(CartContext);
+    const [btnAnimating, setBtnAnimating] = useState(false);
+
     const cartItemsCount = cartCtx.items.reduce((currCount, item) => {
         return currCount + item.amount;
     }, 0);
 
+    //Cart bump animation logic
+    const btnClasses = `${classes.button} ${btnAnimating ? classes.bump : '' }`;
+    useEffect( () => {
+        if (cartCtx.items.length === 0) {
+            return;
+        }
+        setBtnAnimating(true);
+        const timer = setTimeout(() => {
+            setBtnAnimating(false);
+        }, 300);
+
+        //Cleanup fn runs if component is removed
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [cartCtx.items]);
+
     return(
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
